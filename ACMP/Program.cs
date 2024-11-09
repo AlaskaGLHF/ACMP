81e774f88e8bd49f7833129168af9f472a1aa871
AlaskaGLHF <vanushaa89@gmail.com>
2024-11-09 12:36:36 +0300

    fix

--- ACMP/Program.cs
+++ ACMP/Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     static void Main()
     {
-        // Чтение входных данных
+       
         var input = Console.ReadLine().Split();
         int n = int.Parse(input[0]);
         int k = int.Parse(input[1]);
@@ -16,6 +16,7 @@ class Program
 
         var result = BlockSort(n, k, permutation);
         Console.WriteLine(string.Join(" ", result));
+        
     }
 
     static List<int> BlockSort(int n, int k, int[] permutation)
@@ -28,22 +29,19 @@ class Program
         {
             currentLength++;
             maxIndex = Math.Max(maxIndex, permutation[i]);
-
-            // Если maxIndex совпадает с текущей позицией (i + 1)
+            
             if (maxIndex == i + 1)
             {
                 blockLengths.Add(currentLength);
                 currentLength = 0;
             }
         }
-
-        // Проверяем, можем ли мы разбить на k блоков
+        
         if (blockLengths.Count < k || (blockLengths.Count == k && currentLength > 0))
         {
             return new List<int> { -1 };
         }
-
-        // Если больше блоков, чем нужно, то мы можем объединить последние блоки
+        
         while (blockLengths.Count > k)
         {
             blockLengths[blockLengths.Count - 2] += blockLengths[blockLengths.Count - 1];
